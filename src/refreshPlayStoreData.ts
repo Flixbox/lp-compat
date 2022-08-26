d9d4fdae4a448f6e44d1462577d93615f1405a4c
Felix <felix@tietjen.it>
2022-08-26 17:30:51 +0200

    feat: implement data fetching from playstore

--- src/refreshPlayStoreData.ts
+++ src/refreshPlayStoreData.ts
@@ -1,5 +1,26 @@
-const gplay = require("google-play-scraper");
+import gplay from "google-play-scraper";
+import { writeFileSync } from "fs";
+import apps from "../static/compat-data/apps.json";
+// import playstore from "../static/compat-data/apps.json";
 
-gplay
-  .app({ appId: "com.google.android.apps.translate" })
-  .then(console.log, console.log);
+const fetchData = async () => {
+  const playstore = {};
+
+  gplay
+    .app({ appId: "com.google.android.apps.translate" })
+    .then(console.log, console.log);
+
+  for (const appId in apps) {
+    // console.log(`${app}: ${apps[app]}`);
+    const result = await gplay.app({ appId });
+    playstore[appId] = result;
+  }
+
+  console.log(playstore);
+  writeFileSync(
+    "./static/compat-data/playstore.json",
+    JSON.stringify(playstore)
+  );
+};
+
+fetchData();
