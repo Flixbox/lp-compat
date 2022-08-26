644a367bb4f12dd185b0ab888fab6f93b052cd39
Felix <felix@tietjen.it>
2022-08-26 17:56:41 +0200

    feat: add images

--- src/components/CompatOverview/index.tsx
+++ src/components/CompatOverview/index.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 import styles from "./styles.module.css";
 import apps from "../../../static/compat-data/apps.json";
 import playstore from "../../../static/compat-data/playstore.json";
-import { Card, Typography, CardContent, Grid } from "@mui/material";
+import { Card, Typography, CardContent, Grid, Avatar } from "@mui/material";
 
 export default function CompatOverview(): JSX.Element {
   return (
@@ -11,15 +11,20 @@ export default function CompatOverview(): JSX.Element {
       <div className="container">
         <div className="row">
           <Grid container>
-            {Object.entries(apps).map(([appId, { iap }]) => (
-              <Grid item margin={1}>
-                <Card>
-                  <CardContent>
-                    {appId} {iap}
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
+            {Object.entries(apps).map(([appId, { iap }]) => {
+              const { title, icon } = playstore[appId];
+              return (
+                <Grid item margin={1}>
+                  <Card>
+                    <CardContent>
+                      <Avatar src={icon} variant="square"></Avatar>
+                      <Typography>{appId}</Typography>
+                      {iap}
+                    </CardContent>
+                  </Card>
+                </Grid>
+              );
+            })}
           </Grid>
         </div>
       </div>
