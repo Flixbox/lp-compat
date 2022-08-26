e7da43ca9931a2ec526a4a721b0511cc05886943
Felix <felix@tietjen.it>
2022-08-26 17:47:02 +0200

    feat: add grid for apps

--- src/components/CompatOverview/index.tsx
+++ src/components/CompatOverview/index.tsx
@@ -3,17 +3,24 @@ import clsx from "clsx";
 import styles from "./styles.module.css";
 import apps from "../../../static/compat-data/apps.json";
 import playstore from "../../../static/compat-data/playstore.json";
+import { Card, Typography, CardContent, Grid } from "@mui/material";
 
 export default function CompatOverview(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {Object.entries(apps).map(([appId, { iap }]) => (
-            <p>
-              {appId} {iap}
-            </p>
-          ))}
+          <Grid container>
+            {Object.entries(apps).map(([appId, { iap }]) => (
+              <Grid item margin={1}>
+                <Card>
+                  <CardContent>
+                    {appId} {iap}
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </div>
       </div>
     </section>
