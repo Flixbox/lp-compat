5fb3bb6272a26628bd6490997ceada5f75ce9857
Felix <felix@tietjen.it>
2022-08-29 16:35:22 +0200

    feat: enhance app data scraper; add more apps

--- src/refreshPlayStoreData.ts
+++ src/refreshPlayStoreData.ts
@@ -1,7 +1,7 @@
 import gplay from "google-play-scraper";
 import { writeFileSync } from "fs";
 import apps from "../static/compat-data/apps.json";
-// import playstore from "../static/compat-data/playstore.json";
+import oldPlayStoreData from "../static/compat-data/playstore.json";
 
 const fetchData = async () => {
   const playstore = {};
@@ -9,16 +9,20 @@ const fetchData = async () => {
   for (const appId in apps) {
     // console.log(`${app}: ${apps[app]}`);
     try {
-      const result = await gplay.app({ appId });
-      playstore[appId] = result;
-      await new Promise((f) => setTimeout(f, 300));
+      if (oldPlayStoreData[appId] && oldPlayStoreData[appId].title) {
+        playstore[appId] = oldPlayStoreData[appId];
+      } else {
+        const result = await gplay.app({ appId });
+        playstore[appId] = result;
+        await new Promise((f) => setTimeout(f, 300));
+      }
     } catch (e) {
       console.error("App not found: ", appId);
       console.error(e);
     }
   }
 
-  // console.log(playstore);
+  //  console.log(playstore);
   writeFileSync(
     "./static/compat-data/playstore.json",
     JSON.stringify(playstore)
