4a6b21220deb8618db5c0c78070ba554a106db27
Felix Tietjen <felix@tietjen.it>
2022-09-06 01:19:07 +0200

    add discord update feature with webhook

---
+++ src/featureMap.ts
@@ -0,0 +1,143 @@
+import { createTheme } from "@mui/material";
+
+const featureMap = (theme = createTheme()) => ({
+  iap: {
+    label: "IAP patch works!",
+    color: theme.palette.success.main,
+  },
+  "partial-iap": {
+    label: "IAP partially compatible",
+    color: theme.palette.warning.main,
+  },
+  "unclear-iap": {
+    label: "IAP need verification - Try it out and post in Discord!",
+    color: theme.palette.warning.main,
+  },
+  "no-iap": {
+    label: "IAP incompatible",
+    color: theme.palette.error.main,
+  },
+  subscription: {
+    label: "Subscription redeemable!",
+    color: theme.palette.success.main,
+  },
+  "no-subscription": {
+    label: "Subscription cannot be redeemed",
+    color: theme.palette.warning.main,
+  },
+  repurchase: {
+    label: "Some IAPs need to be redeemed after every restart",
+    color: theme.palette.warning.main,
+  },
+  "account-login": {
+    label: "Account login works!",
+    color: theme.palette.info.main,
+  },
+  "facebook-login": {
+    label: "Facebook login works!",
+    color: theme.palette.info.main,
+  },
+  "facebook-login-broken": {
+    label: "Facebook login broken",
+    color: theme.palette.warning.main,
+  },
+  "facebook-login-no-app-installed": {
+    label: "Facebook app must not be installed to login",
+    color: theme.palette.warning.main,
+  },
+  transfer: {
+    label: "App data can be transferred to another account",
+    color: theme.palette.info.main,
+  },
+  multiplayer: {
+    label: "Full Multiplayer; IAP compatible",
+    color: theme.palette.primary.main,
+  },
+  "partial-multiplayer": {
+    label: "App has some multiplayer features; IAP compatible",
+    color: theme.palette.primary.main,
+  },
+  "no-multiplayer": {
+    label: "Singleplayer only",
+    color: theme.palette.warning.main,
+  },
+  "no-multiplayer-iap": {
+    label: "Multiplayer IAP broken",
+    color: theme.palette.warning.main,
+  },
+  "slightly-broken": {
+    label: "Slightly broken, but playable",
+    color: theme.palette.warning.main,
+  },
+  "too-many-iap-break": {
+    label: "Buying too many IAP will break the app!",
+    color: theme.palette.warning.main,
+  },
+  "disable-data-on-lp-popup": {
+    label:
+      "Disable data connection when the LP purchase popup shows to get IAP",
+    color: theme.palette.info.main,
+  },
+  "restore-purchase": {
+    label: "IAP can be redeemed by restoring purchase and restarting the app",
+    color: theme.palette.info.main,
+  },
+  "iap-before-load": {
+    label: "Purchase IAP before the app is fully loaded. Play anonymously.",
+    color: theme.palette.info.main,
+  },
+  "subscription-restore-purchase": {
+    label: "Subscription can be redeemed by restoring purchase",
+    color: theme.palette.info.main,
+  },
+  "dont-bother": {
+    label: "Don't bother. This game is either grindy or uninteresting.",
+    color: theme.palette.warning.main,
+  },
+  "region-locked": {
+    label: "If region locked, use Google Account from another region",
+    color: theme.palette.warning.main,
+  },
+  "patch-before-first-launch": {
+    label: "Patch this game before launching it for the first time!",
+    color: theme.palette.warning.main,
+  },
+  "special-patch-full-offline": {
+    label: "Select patch option: Make fully offline",
+    color: theme.palette.info.main,
+  },
+  "special-patch-signature": {
+    label: "Select patch option: Signature verification killer",
+    color: theme.palette.info.main,
+  },
+  "special-patch-lvl": {
+    label: "Select patch option: Support patch for LVL and Inapp emulation",
+    color: theme.palette.info.main,
+  },
+  "may-require-root": {
+    label: "This app may require a root-level patch to work.",
+    color: theme.palette.info.main,
+  },
+  "root-iap": {
+    label: "IAP patch works with rooted device",
+    color: theme.palette.info.main,
+  },
+  "root-patch": {
+    label: "Root patch available",
+    color: theme.palette.info.main,
+  },
+  "root-patch-iap": {
+    label: "Patch app with root to make IAP work!",
+    color: theme.palette.info.main,
+  },
+  "no-root-patch-iap": {
+    label: "Root patch doesn't make IAP available",
+    color: theme.palette.info.main,
+  },
+  "apk-platinmods": {
+    label: "Patched APK download at PlatinMods",
+    color: theme.palette.info.main,
+  },
+});
+
+export default featureMap;
--- src/sendDiscordUpdate.ts
+++ src/sendDiscordUpdate.ts
@@ -1,12 +1,83 @@
 var exec = require("child_process").exec;
+import { AttachmentBuilder, EmbedBuilder, WebhookClient } from "discord.js";
+import featureMap from "./featureMap";
+const playstore = require("../static/compat-data/playstore.json");
+
+const hook = new WebhookClient({
+  url: process.env.DISCORD_WEBHOOK,
+});
+const logoPath = "./static/img/favicon.ico";
+const logo = new AttachmentBuilder(logoPath);
+
+const featureMapInitialized = featureMap();
+
 function execute(command, callback) {
   exec(command, function (error, stdout, stderr) {
     callback(stdout, error, stderr);
   });
 }
 
-execute("git diff HEAD~ -- ./static/apps.json", (stdout, error, stderr) => {
-  console.log(error);
-  console.log(stdout);
-  console.log(stderr);
-});
+execute(
+  "git diff HEAD~ -- ./static/compat-data/apps.json",
+  (stdout, error, stderr) => {
+    console.error(error);
+    console.error(stderr);
+    console.log(stdout);
+    if (stdout) {
+      const lines = stdout.split("\n");
+      lines.forEach((line) => {
+        const fullLineRegex = /\+  \"(.*?)\"(.*)+/g;
+        const featuresRegex = /(?:\"features\":)(?:\[)(.*)(?:\"\])/g;
+        const result = fullLineRegex.exec(line);
+        if (!result) return;
+        const appId = result[1];
+        console.log("RESULT FOUND - appId: ", appId);
+        const featuresResult = featuresRegex.exec(line);
+        if (!featuresResult) return;
+        const features = featuresResult[1].replace(/\"/g, "").split(",");
+        console.log("RESULT FOUND - featuresResult: ", features);
+
+        const {
+          title,
+          icon,
+          installs,
+          scoreText,
+          url,
+          genre,
+          screenshots,
+          free,
+          priceText,
+        } = playstore[appId];
+
+        let featuresString =
+          "Website Update will be deployed in about 60 seconds.\nCompatibility:";
+        features.forEach(
+          (feature) =>
+            (featuresString += `\n${featureMapInitialized[feature].label}`)
+        );
+
+        let color = 0x0099ff;
+        if (features.indexOf("iap") > -1) color = 0x388e3c;
+        if (features.indexOf("no-iap") > -1) color = 0xf44336;
+        if (features.indexOf("unclear-iap") > -1) color = 0xf57c00;
+
+        const myEmbed = new EmbedBuilder()
+          .setColor(color)
+          .setTitle(title)
+          .setURL(url)
+          .setAuthor({
+            name: "App updated on Compatibility List",
+            url: "https://flixbox.github.io/lp-compat/",
+          })
+          .setDescription(featuresString)
+          .setThumbnail(icon)
+          .setTimestamp()
+          .setFooter({
+            text: "Got more compatible apps? Post them on this Discord and ping @Flixbox!",
+          });
+
+        hook.send({ embeds: [myEmbed] });
+      });
+    }
+  }
+);
