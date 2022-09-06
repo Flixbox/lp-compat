69f9ba8bcbd218c347c64dfe08b914b80da016df
Felix Tietjen <felix@tietjen.it>
2022-09-06 10:02:11 +0200

    make things prettier

--- src/sendDiscordUpdate.ts
+++ src/sendDiscordUpdate.ts
@@ -47,62 +47,61 @@ const main = async () => {
   console.error(error);
   console.error(stderr);
   console.log(stdout);
-  if (stdout) {
-    const lines = stdout.split("\n");
-    lines.forEach((line) => {
-      const fullLineRegex = /\+  \"(.*?)\"(.*)+/g;
-      const featuresRegex = /(?:\"features\":)(?:\[)(.*)(?:\"\])/g;
-      const result = fullLineRegex.exec(line);
-      if (!result) return;
-      const appId = result[1];
-      console.log("RESULT FOUND - appId: ", appId);
-      const featuresResult = featuresRegex.exec(line);
-      if (!featuresResult) return;
-      const features = featuresResult[1].replace(/\"/g, "").split(",");
-      console.log("RESULT FOUND - featuresResult: ", features);
+  if (!stdout) return;
+  const lines = stdout.split("\n");
+  lines.forEach((line) => {
+    const fullLineRegex = /\+  \"(.*?)\"(.*)+/g;
+    const featuresRegex = /(?:\"features\":)(?:\[)(.*)(?:\"\])/g;
+    const result = fullLineRegex.exec(line);
+    if (!result) return;
+    const appId = result[1];
+    console.log("RESULT FOUND - appId: ", appId);
+    const featuresResult = featuresRegex.exec(line);
+    if (!featuresResult) return;
+    const features = featuresResult[1].replace(/\"/g, "").split(",");
+    console.log("RESULT FOUND - featuresResult: ", features);
 
-      const {
-        title,
-        icon,
-        installs,
-        scoreText,
-        url,
-        genre,
-        screenshots,
-        free,
-        priceText,
-      } = playstore[appId];
+    const {
+      title,
+      icon,
+      installs,
+      scoreText,
+      url,
+      genre,
+      screenshots,
+      free,
+      priceText,
+    } = playstore[appId];
 
-      let featuresString =
-        "Website Update will be deployed in about 60 seconds.\nCompatibility:";
-      features.forEach(
-        (feature) =>
-          (featuresString += `\n${featureMapInitialized[feature].label}`)
-      );
+    let featuresString =
+      "Website Update will be deployed in about 60 seconds.\nCompatibility:";
+    features.forEach(
+      (feature) =>
+        (featuresString += `\n${featureMapInitialized[feature].label}`)
+    );
 
-      let color = 0x0099ff;
-      if (features.indexOf("iap") > -1) color = 0x388e3c;
-      if (features.indexOf("no-iap") > -1) color = 0xf44336;
-      if (features.indexOf("unclear-iap") > -1) color = 0xf57c00;
+    let color = 0x0099ff;
+    if (features.indexOf("iap") > -1) color = 0x388e3c;
+    if (features.indexOf("no-iap") > -1) color = 0xf44336;
+    if (features.indexOf("unclear-iap") > -1) color = 0xf57c00;
 
-      const myEmbed = new EmbedBuilder()
-        .setColor(color)
-        .setTitle(title)
-        .setURL(url)
-        .setAuthor({
-          name: "App updated on Compatibility List",
-          url: "https://flixbox.github.io/lp-compat/",
-        })
-        .setDescription(featuresString)
-        .setThumbnail(icon)
-        .setTimestamp()
-        .setFooter({
-          text: "Got more compatible apps? Post them on this Discord and ping @Flixbox!",
-        });
+    const myEmbed = new EmbedBuilder()
+      .setColor(color)
+      .setTitle(title)
+      .setURL(url)
+      .setAuthor({
+        name: "App updated on Compatibility List",
+        url: "https://flixbox.github.io/lp-compat/",
+      })
+      .setDescription(featuresString)
+      .setThumbnail(icon)
+      .setTimestamp()
+      .setFooter({
+        text: "Got more compatible apps? Post them on this Discord and ping @Flixbox!",
+      });
 
-      hook.send({ embeds: [myEmbed] });
-    });
-  }
+    hook.send({ embeds: [myEmbed] });
+  });
 };
 
 main();
