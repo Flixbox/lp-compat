50b6342fd434208094bccb174c41da54c7971d44
Felix Tietjen <felix@tietjen.it>
2022-09-10 23:31:06 +0200

    add to todo list

--- src/components/CompatOverview/index.tsx
+++ src/components/CompatOverview/index.tsx
@@ -39,10 +39,10 @@ import { useColorMode } from "@docusaurus/theme-common";
 
 /*
  * TODO
- * Send a proper update to discord webhook with details
- * find a way to send all updates since last ci run, compare 2 commits - maybe with a "update: " commit tag
- * fix head error thing in CI https://github.com/Flixbox/lp-compat/runs/8197172186?check_suite_focus=true
- * maybe implement a way for ppl to add games (discord bot? form?)
+ * Automated weekly changelog for reddit (crossposted to Discord via link)
+ * Every Changelog has #WW.YY
+ * the WW is weekend, YY is year
+ * So, like week 42, year 22 (42.22)
  */
 
 const Root = () => {
