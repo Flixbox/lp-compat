c293dd01ae18da06e0d936809e32143fb5c4f166
Felix Tietjen <felix@tietjen.it>
2022-09-17 15:16:47 +0200

    finalize app add form

--- src/pages/add-app.tsx
+++ src/pages/add-app.tsx
@@ -24,49 +24,71 @@ const AddApp = () => {
 
   let codeBlock = "";
   appPackages.forEach(
-    (appPackage) => (codeBlock = `${codeBlock}${appPackage}\n`)
+    (appPackage) =>
+      (codeBlock = `${codeBlock}  "${appPackage.appId}":{"features":["iap"]},\n`)
   );
 
-  const handleSubmit = () => setAppPackages([...appPackages, enteredPackage]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAppPackages([...appPackages, { appId: enteredPackage }]);
+  };
+
+  const handleDelete = (appId) =>
+    setAppPackages(
+      appPackages.filter((appPackage) => appPackage.appId !== appId)
+    );
+
   return (
     <Box>
       <Container>
         <Box m={3} />
         <Typography variant="h3">Add an app</Typography>
         <Box m={3} />
-        <Box display="flex">
-          <TextField
-            value={enteredPackage}
-            onChange={(event) => setEnteredPackage(event.target.value)}
-            onSubmit={handleSubmit}
-            label="Package name"
-            placeholder="com.ironhidegames.android.kingdomrush"
-            sx={{ flexGrow: 1 }}
-          />
-          <Box m={1} />
-          <Button variant="outlined" onClick={handleSubmit}>
-            <FontAwesomeIcon icon={faAdd} size="lg" />
-          </Button>
-        </Box>
+        <form onSubmit={handleSubmit}>
+          <Box display="flex">
+            <TextField
+              value={enteredPackage}
+              onChange={(event) => setEnteredPackage(event.target.value)}
+              onSubmit={handleSubmit}
+              label="Package name"
+              placeholder="com.ironhidegames.android.kingdomrush"
+              sx={{ flexGrow: 1 }}
+            />
+            <Box m={1} />
+            <Button variant="outlined" onClick={handleSubmit}>
+              <FontAwesomeIcon icon={faAdd} size="lg" />
+            </Button>
+          </Box>
+        </form>
         <Box m={3} />
         <Box>
           <List dense>
             {appPackages.map((appPackage) => (
               <ListItem
                 secondaryAction={
-                  <Button aria-label="delete">
+                  <Button
+                    onClick={() => handleDelete(appPackage.appId)}
+                    aria-label="delete"
+                  >
                     <FontAwesomeIcon icon={faTrash} size="lg" />
                   </Button>
                 }
-                key={appPackage}
+                key={appPackage.appId}
               >
-                <ListItemText primary={appPackage} />
+                <ListItemText primary={appPackage.appId} />
               </ListItem>
             ))}
           </List>
         </Box>
         <Box m={3} />
         <Box>
+          <Typography variant="h4">Prepared code block</Typography>
+          <Typography>
+            Use this code in <code>static/compat-data/apps.json</code>
+          </Typography>
+          <Typography>
+            Customize it after pasting it by adjusting the features
+          </Typography>
           <pre>
             <code>{codeBlock}</code>
           </pre>
