e41662744a0fdf73802f7a3ce0bd285003e18c6c
Felix Tietjen <felix@tietjen.it>
2022-09-18 15:11:10 +0200

    next attempt for filesystem access

--- src/discord/index.js
+++ src/discord/index.js
@@ -5,20 +5,22 @@ import { Client, Intents, Collection } from "discord.js"; // Define Client, Inte
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 }); // Connect to our discord bot.
+
+console.log("cwd", process.cwd());
+console.log(fs.readdirSync("src/discord/commands"));
+
 const commands = new Collection(); // Where the bot (slash) commands will be stored.
 const commandarray = []; // Array to store commands for sending to the REST API.
 const token = process.env.DISCORD_TOKEN; // Token from Railway Env Variable.
 // Execute code when the "ready" client event is triggered.
 client.once("ready", () => {
-  console.log("cwd", process.cwd());
-
   const commandFiles = fs
-    .readdirSync("/src/discord/commands")
+    .readdirSync("src/discord/commands")
     .filter((file) => file.endsWith(".js")); // Get and filter all the files in the "Commands" Folder.
 
   // Loop through the command files
   for (const file of commandFiles) {
-    const command = require(`/src/discord/commands${file}`); // Get and define the command file.
+    const command = require(`src/discord/commands${file}`); // Get and define the command file.
     commands.set(command.data.name, command); // Set the command name and file for handler to use.
     commandarray.push(command.data.toJSON()); // Push the command data to an array (for sending to the API).
   }
