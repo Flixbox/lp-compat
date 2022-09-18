abb5cf8b71a02d557f8dd7652e029a720599b6e1
Felix Tietjen <felix@tietjen.it>
2022-09-18 17:09:54 +0200

    add some parameters

--- src/discord/Commands/addapp.ts
+++ src/discord/Commands/addapp.ts
@@ -3,8 +3,33 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("addapp")
-    .setDescription("Add a new app to the list!"),
+    .setDescription("Add a new app to the list!")
+    .addStringOption((option) =>
+      option
+        .setName("packageId")
+        .setDescription(
+          "The package ID can be found in the Lucky Patcher app info or in the Play Store URL of the app."
+        )
+    )
+    .addStringOption((option) =>
+      option
+        .setName("features")
+        .setDescription(
+          "A list of notes under the app. Example 1: no-iap | Example 2: iap,warning::Download only on APKPure"
+        )
+    ),
   execute: async (interaction, client) => {
-    return interaction.reply({ content: `Pong \`${client.ws.ping}ms\` 🏓` });
+    const packageId = interaction.options.getString("packageId");
+    const features = interaction.options.getString("features");
+
+    const respond = (content, ephemeral = false) =>
+      interaction.reply({ content, ephemeral });
+    const error = () =>
+      respond(
+        "Something isn't right. Try again with different parameters.",
+        true
+      );
+
+    if (!packageId || !features) return error();
   },
 };
