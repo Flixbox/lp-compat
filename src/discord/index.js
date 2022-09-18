fed8345208610f4758d816c1fd10f5a5e3e2a990
Felix Tietjen <felix@tietjen.it>
2022-09-18 15:04:45 +0200

    try fix path

--- src/discord/index.js
+++ src/discord/index.js
@@ -11,14 +11,14 @@ const token = process.env.DISCORD_TOKEN; // Token from Railway Env Variable.
 // Execute code when the "ready" client event is triggered.
 client.once("ready", () => {
   console.log("cwd", process.cwd());
-  
+
   const commandFiles = fs
-    .readdirSync("./src/discord/commands")
+    .readdirSync("/src/discord/commands")
     .filter((file) => file.endsWith(".js")); // Get and filter all the files in the "Commands" Folder.
 
   // Loop through the command files
   for (const file of commandFiles) {
-    const command = require(`./src/discord/commands${file}`); // Get and define the command file.
+    const command = require(`/src/discord/commands${file}`); // Get and define the command file.
     commands.set(command.data.name, command); // Set the command name and file for handler to use.
     commandarray.push(command.data.toJSON()); // Push the command data to an array (for sending to the API).
   }
