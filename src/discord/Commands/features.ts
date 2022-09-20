a89f4df8e8478fba0aef172d4740d5532c0db2a0
Felix Tietjen <felix@tietjen.it>
2022-09-20 22:39:36 +0200

    add some more detail to features info

--- src/discord/Commands/features.ts
+++ src/discord/Commands/features.ts
@@ -7,10 +7,21 @@ module.exports = {
     .setDescription("Show the features that you can add to an app"),
   execute: async (interaction, client) => {
     let textResponse = `Available features:`;
-    const features = featureMap()
+    const features = featureMap();
     Object.keys(features).forEach(
-      (feature) => (textResponse = `${textResponse}\n${feature}\n*${features[feature].label}*`)
+      (feature) =>
+        (textResponse = `${textResponse}\n${feature}\n*${features[feature].label}*`)
     );
+    textResponse = `${textResponse}
+
+You can also create custom features using this syntax:
+
+\`::Download version 1.2.3 on APKPure; then patch\`
+
+\`warning::Don't click the fake ads - Please use AdGuard\`
+
+\`success::IAP work with some minor restrictions\`
+`;
 
     return await interaction.editReply(textResponse);
   },
