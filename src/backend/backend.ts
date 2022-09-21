7c7d14dc950935394f841286a335e6f458e5757b
Felix Tietjen <felix@tietjen.it>
2022-09-21 23:29:17 +0200

    fix type

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -5,7 +5,7 @@ import getApp from "../db/getApp";
 const apps = require("../../static/compat-data/apps.json");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = +process.env.PORT || 5000;
 const hostname = process.env.HOSTNAME || "localhost";
 
 (async () => {
