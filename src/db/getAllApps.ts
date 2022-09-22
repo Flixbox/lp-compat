6875752f7ec6e37b957715756efa48ffc553670d
Felix Tietjen <felix@tietjen.it>
2022-09-22 13:02:27 +0200

    adjust projection for app list

--- src/db/getAllApps.ts
+++ src/db/getAllApps.ts
@@ -2,6 +2,27 @@ import { executeAppsQuery } from "./util";
 
 export default async () => {
   return await executeAppsQuery(
-    async (appsCollection) => await appsCollection.find().toArray()
+    async (appsCollection) =>
+      await appsCollection
+        .find(
+          {},
+          {
+            projection: {
+              appId: 1,
+              features: 1,
+              dateModified: 1,
+              title: 1,
+              icon: 1,
+              installs: 1,
+              scoreText: 1,
+              url: 1,
+              genre: 1,
+              screenshots: 1,
+              free: 1,
+              priceText: 1,
+            },
+          }
+        )
+        .toArray()
   );
 };
