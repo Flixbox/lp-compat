fe55cd43a58a61845c2877a12db8d1818e2a7db4
Felix Tietjen <felix@tietjen.it>
2022-09-23 19:34:47 +0200

    maybe fix cors

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -15,14 +15,14 @@ const app = express();
 const port = +process.env.PORT || 5000;
 const hostname = process.env.HOSTNAME || "localhost";
 
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(generatedDocs, { explorer: true })
 );
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
