b2a6575bfaaf330602fe15547d81e267ceb2187e
Felix Tietjen <f.tietjen@clevershuttle.de>
2023-03-21 17:37:01 +0100

    details

--- src/discord/Commands/cse.ts
+++ src/discord/Commands/cse.ts
@@ -14,6 +14,10 @@ You can find those under: Wiki (pinned post) > Android / iOS > Android APKs.
 Remember to use VirusTotal to check the downloaded file for viruses. 
 If it's over 650MB, you can use the Avast app to check the file locally. 
 Direct link: [Android APKs](https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/android/#wiki_.25BA_android_apks)
+
+If you can't find the app in the CSE:
+- If it's a multiplayer app, it's likely that the server-side logic would require the modder to implement a private server. Example: [nulls.gg](https://nulls.gg) - This is not possible for most smaller apps.
+- If it's a singleplayer app, it's likely that the app just hasn't been patched yet. This can have several reasons - Strong anticheat, signature verification, or simply that no one has been interested in patching it. You can suggest in in the #mod-discussion channel.
       `
     );
   },
