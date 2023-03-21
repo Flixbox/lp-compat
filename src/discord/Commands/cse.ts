4d9b0a04f4d5cbe194f6185558efc5f4c568e338
Felix Tietjen <f.tietjen@clevershuttle.de>
2023-03-21 16:47:18 +0100

    add cse info

---
+++ src/discord/Commands/cse.ts
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("cse")
+    .setDescription(
+      "Explain the CSE (Custom Search Engine) for modded APKs and how to use it."
+    ),
+  execute: async (interaction) => {
+    return await interaction.followUp(
+      `
+To check for an app that is incompatible with Lucky Patcher, you can visit the modded APK CSE in [r/freemediaheckyeah](https://www.reddit.com/r/freemediaheckyeah/). 
+You can find the APK files that have been manually modified under: Wiki (pinned post) > Android / iOS > Android APKs. 
+Remember to use VirusTotal to check the downloaded file for viruses. 
+If it's over 650MB, you can use the Avast app to check the file locally. 
+Direct link: [Android APKs](https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/android/#wiki_.25BA_android_apks)
+      `
+    );
+  },
+};
