8c7e16e8487324c26ed907a82537c2b88313a602
Felix Tietjen <f.tietjen@clevershuttle.de>
2023-03-21 16:56:41 +0100

    clarification

--- src/discord/Commands/cse.ts
+++ src/discord/Commands/cse.ts
@@ -9,8 +9,8 @@ module.exports = {
   execute: async (interaction) => {
     return await interaction.followUp(
       `
-To check for an app that is incompatible with Lucky Patcher, you can visit the modded APK CSE in [r/freemediaheckyeah](https://www.reddit.com/r/freemediaheckyeah/). 
-You can find the APK files that have been manually modified under: Wiki (pinned post) > Android / iOS > Android APKs. 
+To find a modded APK for an app that is incompatible with Lucky Patcher, you can visit the modded APK CSE in [r/freemediaheckyeah](https://www.reddit.com/r/freemediaheckyeah/). 
+You can find those under: Wiki (pinned post) > Android / iOS > Android APKs. 
 Remember to use VirusTotal to check the downloaded file for viruses. 
 If it's over 650MB, you can use the Avast app to check the file locally. 
 Direct link: [Android APKs](https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/android/#wiki_.25BA_android_apks)
