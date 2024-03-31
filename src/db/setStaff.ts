b954ad3cc155c2496e07f905ae6dbb991e6456e0
Felix Tietjen <felix@tietjen.it>
2024-03-31 13:12:29 +0200

    refactor: fix insert logic

--- src/db/setStaff.ts
+++ src/db/setStaff.ts
@@ -4,6 +4,6 @@ import { Collection, GuildMember } from "discord.js";
 export default async (staff: Collection<string, GuildMember>) => {
   return await executeStaffQuery(async (staffCollection) => {
     await staffCollection.deleteMany({});
-    await staffCollection.insertMany(staff);
+    await staffCollection.insertMany(staff.map((member) => ({ ...member })));
   });
 };
