c23303c84918dcf4a06058bd8797de90dd42e0fe
Felix Tietjen <felix@tietjen.it>
2024-04-01 14:06:41 +0200

    refactor: make auth more safe

--- src/hooks/useDiscord.ts
+++ src/hooks/useDiscord.ts
@@ -7,26 +7,40 @@ export type DiscordUser = {
   id: string;
 };
 
+export type DiscordUserAuth = {
+  accessToken: string;
+  tokenType: string;
+};
+
 // TODO Clean URL bar and move token to persistent state if available
 export const useDiscord = () => {
-  const [storedDiscordUser, setStoredDiscordUser] = usePersistentState<
-    DiscordUser | undefined
-  >(undefined, { storageKey: "discordUser" });
+  const [storedDiscordUserAuth, setStoredDiscordUserAuth] = usePersistentState<
+    DiscordUserAuth | undefined
+  >(undefined, { storageKey: "discordUserAuth" });
 
-  let accessToken;
-  let tokenType;
   if (ExecutionEnvironment.canUseDOM) {
+    let accessToken;
+    let tokenType;
     const fragment = new URLSearchParams(window.location.hash.slice(1));
     [accessToken, tokenType] = [
       fragment.get("access_token"),
       fragment.get("token_type"),
     ];
+    if (accessToken && tokenType) {
+      setStoredDiscordUserAuth({ accessToken, tokenType });
+
+      // Clear sensitive token from address bar
+      const newUrl = window.location.href.split("#")[0];
+      location.replace(newUrl);
+    }
   }
   const { data: discordUser } = useQuery<DiscordUser>("discord", async () =>
     (
       await fetch("https://discord.com/api/v9/users/@me", {
         method: "GET",
-        headers: { authorization: `${tokenType} ${accessToken}` },
+        headers: {
+          authorization: `${storedDiscordUserAuth.tokenType} ${storedDiscordUserAuth.accessToken}`,
+        },
       })
     ).json()
   );
