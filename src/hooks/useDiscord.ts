31052eb8ef513a081c049db906d08bb2bca8696b
Felix Tietjen <felix@tietjen.it>
2024-04-01 15:14:18 +0200

    refactor: wrap code properly in effect

--- src/hooks/useDiscord.ts
+++ src/hooks/useDiscord.ts
@@ -1,6 +1,7 @@
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import { useLocalStorage } from "usehooks-ts";
 import { useQuery } from "react-query";
+import { useEffect } from "react";
 
 export type DiscordUser = {
   username: string;
@@ -17,31 +18,34 @@ export const useDiscord = () => {
   console.log("storedDiscordUserAccessToken", storedDiscordUserAccessToken);
   console.log("storedDiscordUserTokenType", storedDiscordUserTokenType);
 
-  if (ExecutionEnvironment.canUseDOM) {
-    let accessToken;
-    let tokenType;
-    const fragment = new URLSearchParams(window.location.hash.slice(1));
-    [accessToken, tokenType] = [
-      fragment.get("access_token"),
-      fragment.get("token_type"),
-    ];
-    if (accessToken && tokenType) {
-      console.log("Setting stored auth ", { accessToken, tokenType });
-      setStoredDiscordUserAccessToken(accessToken);
-      setStoredDiscordUserTokenType(tokenType);
+  useEffect(() => {
+    if (ExecutionEnvironment.canUseDOM) {
+      let accessToken;
+      let tokenType;
+      const fragment = new URLSearchParams(window.location.hash.slice(1));
+      [accessToken, tokenType] = [
+        fragment.get("access_token"),
+        fragment.get("token_type"),
+      ];
 
-      if (
-        storedDiscordUserAccessToken &&
-        storedDiscordUserTokenType &&
-        localStorage.getItem("storedDiscordUserAccessToken") !== '""' &&
-        localStorage.getItem("storedDiscordUserTokenType") !== '""'
-      ) {
-        // Clear sensitive token from address bar
-        const newUrl = window.location.href.split("#")[0];
-        location.replace(newUrl);
+      if (accessToken && tokenType) {
+        console.log("Setting stored auth ", { accessToken, tokenType });
+        setStoredDiscordUserAccessToken(accessToken);
+        setStoredDiscordUserTokenType(tokenType);
+
+        if (
+          storedDiscordUserAccessToken &&
+          storedDiscordUserTokenType &&
+          localStorage.getItem("storedDiscordUserAccessToken") !== '""' &&
+          localStorage.getItem("storedDiscordUserTokenType") !== '""'
+        ) {
+          // Clear sensitive token from address bar
+          const newUrl = window.location.href.split("#")[0];
+          location.replace(newUrl);
+        }
       }
     }
-  }
+  }, [storedDiscordUserAccessToken, storedDiscordUserTokenType]);
 
   const { data: discordUser } = useQuery<DiscordUser>("discord", async () =>
     (
