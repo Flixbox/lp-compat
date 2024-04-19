c5f5ace48c8d94a16f7138544a3b314312b9d8b2
Felix Tietjen <felix@tietjen.it>
2024-04-19 23:47:50 +0200

    refactor: change directory

---
+++ src/discord/commands/help.ts
@@ -0,0 +1,31 @@
+import { featureMap } from "../../featureMap";
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("help")
+    .setDescription("A comprehensive guide to using the bot."),
+  execute: async (interaction, client) => {
+    return await interaction.editReply(`
+    The Lucky Patcher community bot has the following commands available:
+    **/help** - *Displays this guide.*
+    **/patching** - *Explains how to patch an app and links to the site.*
+    **/addapp** - *Allows you to add an app to the compatibility list. After the app is verified, it will be added to the database.*
+    \`\`\`
+    Syntax: 
+    /upsertapp <package>  <feature>|<feature>|...
+      package: You can either use the package name or the URL to the app on the play store. The package name can be found by tapping "App info" in Lucky Patcher.
+      features: This is a list of notes on the app, separated by a pipe symbol (|). Do not use pipes in the feature description, and use "::" to prefix any custom notes. You can get a full list of predefined features using the "features" command.
+      
+    Examples: 
+    /upsertapp com.gramgames.mergedragons iap|::Works with version 1.2.3 from APKPure 
+    /upsertapp games.onebutton.golfbattle no-iap 
+    /upsertapp https://play.google.com/store/apps/details?id=com.gramgames.mergedragons iap|facebook-login
+    \`\`\`
+
+    **/features** - *Displays a list of predefined features that can be used with the "upsertapp" command.*
+
+    __To use these commands, make sure to use Slash Commands by typing "/" into the chat bar and selecting the desired command. Simply typing the command without the slash (e.g. "addapp") will not work.__
+    `);
+  },
+};
---
+++ src/discord/commands/upsertapp.ts
@@ -0,0 +1,118 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+import {
+  isStaff,
+  processFeatures,
+  processPackage,
+  validatePackage,
+} from "../util";
+import addApp from "../../db/addApp";
+import editApp from "../../db/editApp";
+import { App } from "../../types";
+import getPlaystoreData from "../../backend/getPlaystoreData";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("upsertapp")
+    .setDescription("Upsert (update or insert) a new app to the list!")
+    .addStringOption((option) =>
+      option
+        .setName("package")
+        .setDescription(
+          "/help - Use the app's package name (e.g. com.gramgames.mergedragons) or the URL to the app."
+        )
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("features")
+        .setDescription(
+          "/help - Use the predefined /features or create custom ones using the format ::<custom feature>."
+        )
+        .setRequired(true)
+    ),
+
+  execute: async (interaction, client) => {
+    let packageParam: string = interaction.options.getString("package");
+    let featuresString: string = interaction.options.getString("features");
+
+    const response = (content, ephemeral = false) =>
+      interaction.editReply({ content, ephemeral });
+    const error = (
+      e = "Something isn't right. Please try again with different parameters."
+    ) => response(e, true);
+
+    /*
+    if (!isStaff)
+      return await error(
+        "You don't have the `Compatibility List Manager` role. Sorry!"
+      );
+    */
+
+    if (!packageParam) return await error();
+    if (!featuresString) return await error();
+
+    let features: ReturnType<typeof processFeatures>;
+    let appId;
+
+    const featuresError = async () =>
+      error(
+        "There is an issue with the list of features you provided. Please refer to the `/help` command for guidance."
+      );
+
+    try {
+      features = processFeatures(featuresString);
+    } catch (e) {
+      return await featuresError();
+    }
+
+    if (!features) return await featuresError();
+
+    try {
+      appId = await processPackage(packageParam);
+      if (!appId) throw new Error();
+    } catch (e) {
+      return await error(
+        "There is an issue with the app package you provided. Make sure it is written correctly, or try using the URL to the app on the play store. You can also refer to the `/help` command for assistance."
+      );
+    }
+
+    try {
+      await getPlaystoreData(appId);
+    } catch (e) {
+      return await error(
+        "The app you provided cannot be found in the specified play store region. Please try again with a different app or region."
+      );
+    }
+
+    try {
+      await addApp(
+        { appId, features } as App,
+        interaction.user.tag,
+        interaction.user.id
+      );
+    } catch (e) {
+      // The app is probably already on the list. Let's try to update it.
+      try {
+        await editApp(
+          { appId, features } as App,
+          interaction.user.tag,
+          interaction.user.id
+        );
+      } catch (e) {
+        return await error(
+          `
+          The app package ${appId} could not be updated. 
+          Provided features: ${features.toString()}
+          `
+        );
+      }
+    }
+
+    let textResponse = `Upserted (updated/inserted) app ${appId}!\nThanks ${interaction.user.tag}!`;
+    // if (isStaff(interaction))
+    //   textResponse = `${textResponse}\nPR was automatically merged.`;
+
+    return await interaction.editReply(textResponse);
+  },
+};
