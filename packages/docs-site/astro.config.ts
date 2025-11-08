2278dcb4a024c7ecb93943229594651a1b2faecb
Flixbox <felix@tietjen.it>
2025-11-08 18:03:15 +0100

    add various deps and react compiler

--- packages/docs-site/astro.config.ts
+++ packages/docs-site/astro.config.ts
@@ -1,20 +1,33 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import react from '@astrojs/react'
+import mdx from '@astrojs/mdx'
+import babel from 'esbuild-plugin-babel'
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/lp-compat',
-	integrations: [
-		starlight({
-			title: 'LP-Tools',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Flixbox/lp-compat' }],
-			sidebar: [
-				{
-					label: 'Docs',
-					autogenerate: { directory: 'docs', collapsed: false },
-				},
-			],
-		}),
-	],
-});
+  base: '/lp-compat',
+  integrations: [
+    react(),
+    starlight({
+      title: 'LP-Tools',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/Flixbox/lp-compat',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Docs',
+          autogenerate: { directory: 'docs', collapsed: false },
+        },
+      ],
+    }),
+    mdx(),
+  ],
+  vite: {
+    plugins: [babel()],
+  },
+})
