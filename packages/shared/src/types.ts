242f0bc110811243c1d776b8a2b021082638a648
Flixbox <felix@tietjen.it>
2025-11-09 17:02:55 +0100

    fix imports

---
+++ packages/shared/src/types.ts
@@ -0,0 +1,37 @@
+interface App {
+  _id: string;
+  appId: string;
+  features: string[];
+  dateModified: number;
+  title: string;
+  summary: string;
+  installs: string;
+  minInstalls: number;
+  price: number;
+  free: boolean;
+  score: number;
+  scoreText: string;
+  priceText: string;
+  androidVersion: string;
+  androidVersionText: string;
+  developer: string;
+  developerId: string;
+  genre: string;
+  genreId: string;
+  icon: string;
+  headerImage: string;
+  screenshots: string[];
+  adSupported: boolean;
+  updated: number;
+  version: string;
+  recentChanges: string;
+  url: string;
+  offersIAP?: boolean;
+  IAPRange?: string;
+  editedBy: {
+    userName: string;
+    userId: string;
+  };
+}
+
+export type { App };
