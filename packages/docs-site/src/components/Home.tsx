08ad881c7464ac74b192d15276ba958e799d84ef
Flixbox <felix@tietjen.it>
2025-11-09 14:22:16 +0100

    add some old files with deps

---
+++ packages/docs-site/src/components/Home.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import clsx from 'clsx'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
+import CompatOverview from '@site/src/components/CompatOverview'
+import styles from './index.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Box, Link } from '@mui/material'
+import { faFaceSmileWink } from '@fortawesome/free-solid-svg-icons'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+
+config.autoAddCss = false
+
+const LINKS = [
+  { to: '/docs/lp-info', text: 'Important info about LP 💡' },
+  { to: '/docs/intro', text: 'How to install & patch an app 📖' },
+  { to: '/docs/favourites', text: 'Community Favourites ⭐' },
+  { to: '#apps', text: 'To the list 🚀' },
+]
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext()
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <FontAwesomeIcon icon={faFaceSmileWink} size="4x" aria-hidden="true" />
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <Box className={styles.buttons} flex="1" flexDirection="column">
+          {LINKS.map((link, index) => (
+            <React.Fragment key={link.to}>
+              <Link
+                className="button button--secondary button--lg"
+                to={link.to}
+              >
+                {link.text}
+              </Link>
+              {index < LINKS.length - 1 && <Box m={1} />}
+            </React.Fragment>
+          ))}
+        </Box>
+      </div>
+    </header>
+  )
+}
+
+function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext()
+  const pageTitle = `${siteConfig.title}`
+  const pageDescription =
+    'A full overview of Lucky Patcher Compatibility with various apps.'
+
+  return (
+    <Layout title={pageTitle} description={pageDescription}>
+      <HomepageHeader />
+      <main>
+        <CompatOverview />
+      </main>
+    </Layout>
+  )
+}
+
+export { Home }
